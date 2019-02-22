2c9d4dd17be669a7a761ec80e3d908499f4c2965
nabila-mo4 <32497695+nabila-mo4@users.noreply.github.com>
2019-02-22 10:20:10 +0100

    Create CommentaireController.java

---
+++ commentaire/src/main/java/com/ms/commentaire/controller/CommentaireController.java
@@ -0,0 +1,34 @@
+package com.ms.mscommentaire.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.mscommentaire.model.Commentaire;
+import com.ms.mscommentaire.repository.CommentaireDao;
+
+@RestController
+public class CommentaireController {
+	
+	@Autowired
+    CommentaireDao comDao;
+	
+	@GetMapping(value = "/comments/{idaventure}")
+    public List<Commentaire> listbyidaventure(@PathVariable Long idaventure) {
+        
+        List<Commentaire> comments = new ArrayList<>();
+        comments.add(comDao.findByIdaventure(idaventure));
+        return comments;
+    }
+
+	@PostMapping(value = "/commentaire/save")
+    public void save(@RequestBody Commentaire comment) {
+        comDao.save(comment);
+    }
+}
