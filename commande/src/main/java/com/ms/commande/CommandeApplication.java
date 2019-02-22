2ad6248f2012e9fd9ae998e4419103961720cc9a
benhadji <hadjiben@gmail.com>
2019-02-22 17:28:13 +0100

    Mise en place edge MS Eureka - OK

--- commande/src/main/java/com/ms/commande/CommandeApplication.java
+++ commande/src/main/java/com/ms/commande/CommandeApplication.java
@@ -2,8 +2,10 @@ package com.ms.commande;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommandeApplication {
 
 	public static void main(String[] args) {
