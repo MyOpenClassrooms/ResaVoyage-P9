f24bf6be588fcb06b062e6bdbbe6cacccdf52ab4
dikha <khadisankhe@gmail.com>
2019-02-28 00:24:00 +0100

    making admin on others ms

--- admin/src/main/java/com/ms/admin/AdminApplication.java
+++ admin/src/main/java/com/ms/admin/AdminApplication.java
@@ -3,7 +3,9 @@ package com.ms.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
+import brave.sampler.Sampler;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
@@ -14,5 +16,9 @@ public class AdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
 	}
-
+	@Bean
+	Sampler getSample() {
+		return Sampler.ALWAYS_SAMPLE;
+		
+	}
 }
