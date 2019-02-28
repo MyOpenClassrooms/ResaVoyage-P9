256407966414413bc6997630ebf66be01d8c8b1f
TATTOU <nabila.mokadmi@capgemini.com>
2019-03-01 00:03:28 +0100

    debut implementation ms zuul

---
+++ src/main/java/com/ms/model/Utilisateur.java
@@ -0,0 +1,58 @@
+package com.ms.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class Utilisateur implements Serializable , UserDetails {
+
+
+    private String username;
+
+    private String password;
+
+   
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
